520cee88b43cc24150e9780b9a97a568c152a189
chamwabaraka <barakachamwa@gmail.com>
2024-11-12 22:47:21 +0300

    Add files via upload

---
+++ gesture .js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>javascript external script</title>
+<script src=soul"stype="text/javascript"/></script>
+</head>
+<body>
+<input type="button"onclick="hello();name="ok" value="click me"/>
+</body>
+</html>
